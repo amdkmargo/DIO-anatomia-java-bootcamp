e9a3c5f6ecd0e94bd3dce07ac4148690defc253b
Amanda Camargo <amanda_kamargo@hotmail.com>
2024-04-30 21:31:31 -0300

    First Commit

---
+++ src/edu/amanda/anatomiaClasses/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.amanda.anatomiaClasses;
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Amanda";
+        String segundoNome = "Camargo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
